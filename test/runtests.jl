28ca05a4222857ac194261bc4d434d545211b6b1
K Pamnany <kpamnany@users.noreply.github.com>
2025-08-19 17:06:37 -0400

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -142,12 +142,14 @@ end
     sf()
 end
 
+const sval_unassigned = ScopedValue{Int}()
 @testset "get with default" begin
-    const sval_unassigned = ScopedValue{Int}()
     @test ScopedValues.get(sval_unassigned, nothing) == nothing
     @test ScopedValues.get(sval_unassigned, -1) == -1
     @with sval_unassigned=>10 begin
         @test ScopedValues.get(sval_unassigned, -1) == 10
     end
+    @static if VERSION >= v"1.9"
     @test 0 == @allocations ScopedValues.get(sval_unassigned, nothing)
+    end
 end
